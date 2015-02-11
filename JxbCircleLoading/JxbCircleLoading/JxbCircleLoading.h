9bcf56b6044cdd2eb1ce01c74628626b8d971876
Peter <i@jxb.name>
2015-02-11 16:27:03 +0800

    first commit

---
+++ JxbCircleLoading/JxbCircleLoading/JxbCircleLoading.h
@@ -0,0 +1,13 @@
+//
+//  JxbCircleLoading.h
+//  JxbCircleLoading
+//
+//  Created by Peter on 15/2/11.
+//  Copyright (c) 2015年 Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JxbCircleLoading : UIView
+
+@end
---
+++ JxbCircleLoading/JxbCircleLoading/JxbCircleLoading.m
@@ -0,0 +1,21 @@
+//
+//  JxbCircleLoading.m
+//  JxbCircleLoading
+//
+//  Created by Peter on 15/2/11.
+//  Copyright (c) 2015年 Peter. All rights reserved.
+//
+
+#import "JxbCircleLoading.h"
+
+@implementation JxbCircleLoading
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
